7ce9c2054b079b791c8a6633c5ebc0ec03dadde8
Ben_XP <brexp14@gmail.com>
2025-04-06 22:19:45 -0300

    The journey begins

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+     return (
+          <footer>
+               <p>&copy; {new Date().getFullYear()} Little project</p>
+          </footer>
+     );
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+     return(
+          <header>
+               <h1>React project</h1>
+               <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Stories</a></li>
+                    <li><a href="#">About</a></li>
+               </ul>
+               <hr />
+          </header>
+     );
+}
+
+export default Header
